d6a9aad2dc0b8ca8588f467c1ad57ea6ef0a5b96
Aishwarya Bhargava <aish2997@gmail.com>
2024-12-07 16:40:26 +0100

    Fix

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,5 @@
+/*
+
 output "cloud_run_url" {
   description = "The URL of the Cloud Run service"
   value       = google_cloud_run_service.backend.status[0].url
@@ -7,3 +9,5 @@ output "cdn_backend" {
   description = "The Cloud CDN backend URL"
   value       = google_compute_backend_bucket.cdn_backend.bucket_name
 }
+
+*/
