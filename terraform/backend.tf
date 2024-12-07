1d05974297405aba22b10e29d1f154894e0de665
Aishwarya Bhargava <aish2997@gmail.com>
2024-12-07 18:56:57 +0100

    Fix

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,5 +1,5 @@
 backend "gcs" {
-  bucket  = "blog-website-d-state"  # Ensure the bucket exists
+  bucket  = "blog-website-d-state" # Ensure the bucket exists
   prefix  = "terraform/state"
   project = var.project_id
 }
