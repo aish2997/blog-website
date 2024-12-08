d7a0108b80138fdafdb09feeba3d3480ed7db40d
Aishwarya Bhargava <aish2997@gmail.com>
2024-12-08 23:08:36 +0100

    Fix

--- terraform/gcs.tf
+++ terraform/gcs.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "buckets" {
   for_each = var.storage_buckets
 
-  name          = "random-mwmwmwmmwihwefohoewfigho"
-  location      = each.value.location
+  name     = "random-mwmwmwmmwihwefohoewfigho"
+  location = each.value.location
 }
