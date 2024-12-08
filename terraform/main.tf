af05a320c8f5da1dd07c0cb87d72560fd814dfc5
Aishwarya Bhargava <aish2997@gmail.com>
2024-12-09 00:03:00 +0100

    Fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project = "blog-website-d"
-  region  = "europe-west1"
+  project                     = "blog-website-d"
+  region                      = "europe-west1"
   impersonate_service_account = "github-actions-sa@blog-website-d.iam.gserviceaccount.com"
 }
