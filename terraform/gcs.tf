27d89adc867053d296be22dfc3fa8ddd4d8d3979
Aishwarya Bhargava <aish2997@gmail.com>
2024-12-08 23:51:38 +0100

    Fix

--- terraform/gcs.tf
+++ terraform/gcs.tf
@@ -10,6 +10,6 @@ resource "google_storage_bucket" "buckets" {
 
 resource "google_storage_bucket" "example_bucket" {
   name     = "your-unique-bucket-name-29292992" # Follow naming rules
-  location = "EU"                     # Ensure location is valid
-  project  = "blog-website-d"    # Correct project ID
+  location = "EU"                               # Ensure location is valid
+  project  = "blog-website-d"                   # Correct project ID
 }
