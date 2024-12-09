7b58828e84c1ad5c998a32fe82ab86269a07e6ad
Aishwarya Bhargava <aish2997@gmail.com>
2024-12-09 21:32:00 +0100

    Added dns

--- terraform/gcs.tf
+++ terraform/gcs.tf
@@ -4,4 +4,9 @@ resource "google_storage_bucket" "buckets" {
   name          = "${each.key}-${var.environment}"
   location      = each.value.location
   storage_class = each.value.class
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
 }
