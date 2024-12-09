581bc98319e3d78c8e0efd14f7fa1898343098e7
Aishwarya Bhargava <aish2997@gmail.com>
2024-12-09 22:30:49 +0100

    Fix

--- terraform/gcs.tf
+++ terraform/gcs.tf
@@ -1,7 +1,7 @@
-resource "google_storage_bucket" "buckets" {
+resource "google_storage_bucket" "blog_bucket" {
   for_each = var.storage_buckets
 
-  name          = "${each.key}-${var.environment}"
+  name          = "www.aishwaryabhargava.com"
   location      = each.value.location
   storage_class = each.value.class
 
@@ -9,4 +9,35 @@ resource "google_storage_bucket" "buckets" {
     main_page_suffix = "index.html"
     not_found_page   = "404.html"
   }
+  
+  lifecycle {
+    prevent_destroy = false # Allows the bucket to be destroyed if needed
+  }
+
+  cors {
+    origins          = ["*"]
+    methods          = ["GET", "HEAD"]
+    response_headers = ["Content-Type"]
+    max_age_seconds  = 3600
+  }
+}
+
+# Grant 'allUsers' permission to read the contents of the bucket
+resource "google_storage_bucket_object" "public_index" {
+  bucket = google_storage_bucket.blog_bucket.name
+  name   = "index.html"
+  source = "blog-storage-dev/index.html" # Replace with your local file path
+}
+
+resource "google_storage_bucket_object" "public_404" {
+  bucket = google_storage_bucket.blog_bucket.name
+  name   = "404.html"
+  source = "blog-storage-dev/404.html" # Replace with your local file path
+}
+
+resource "google_storage_bucket_iam_binding" "bucket_all_users" {
+  bucket = google_storage_bucket.blog_bucket.name
+
+  role    = "roles/storage.objectViewer"
+  members = ["allUsers"]
 }
