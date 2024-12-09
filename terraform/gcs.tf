471b91284803e04dc27dd02825b14ebe069e36f1
Aishwarya Bhargava <aish2997@gmail.com>
2024-12-09 21:04:16 +0100

    Updated frontend

--- terraform/gcs.tf
+++ terraform/gcs.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "buckets" {
   for_each = var.storage_buckets
 
-  name          = "${each.key}-${var.environment}"
+  name          = "${each.key}"
   location      = each.value.location
   storage_class = each.value.class
 }
