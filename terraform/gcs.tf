9887d68059155aba9987bf57942678ea472a441d
Aishwarya Bhargava <aish2997@gmail.com>
2024-12-11 19:20:14 +0100

    Fix

--- terraform/gcs.tf
+++ terraform/gcs.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "blog_bucket" {
   for_each = var.storage_buckets
 
-  name          = "my-bucket-aishwaryabhargava"
+  name          = "my-bucket-${each.key}"
   location      = each.value.location
   storage_class = each.value.class
 
@@ -13,4 +13,12 @@ resource "google_storage_bucket" "blog_bucket" {
   lifecycle {
     prevent_destroy = false # Allows the bucket to be destroyed if needed
   }
+}
+
+resource "google_storage_bucket_iam_member" "all_users_viewer" {
+  for_each = { for key, bucket in var.storage_buckets : key => bucket if bucket.public_access }
+
+  bucket = google_storage_bucket.blog_bucket[each.key].name
+  role   = "roles/storage.objectViewer"
+  member = "allUsers"
 }
