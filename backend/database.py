f9f8c5abaa5619f33f44564755c61eb5f81888df
Aishwarya Bhargava <aish2997@gmail.com>
2024-12-12 19:59:47 +0100

    Code cleanup

--- backend/database.py
+++
@@ -1,9 +0,0 @@
-from google.cloud import bigquery
-from backend.config import settings
-
-client = bigquery.Client(project=settings.PROJECT_ID)
-
-def execute_query(query, params=None):
-    """Executes a BigQuery query."""
-    job = client.query(query, job_config=bigquery.QueryJobConfig(query_parameters=params or []))
-    return job.result()
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,17 +1,3 @@
-/*
-
-output "cloud_run_url" {
-  description = "The URL of the Cloud Run service"
-  value       = google_cloud_run_service.backend.status[0].url
-}
-
-output "cdn_backend" {
-  description = "The Cloud CDN backend URL"
-  value       = google_compute_backend_bucket.cdn_backend.bucket_name
-}
-
-*/
-
 output "public_buckets" {
   value = [
     for key, bucket in var.storage_buckets :
