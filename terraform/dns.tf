09ce69b56b8efb4d19cda606be137f20ed121acf
Aishwarya Bhargava <aish2997@gmail.com>
2024-12-15 19:04:22 +0100

    DNS Record Fix

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -5,26 +5,24 @@ resource "google_dns_managed_zone" "my_zone" {
   visibility = "public"
 }
 
-# CNAME Record for www subdomain
-resource "google_dns_record_set" "www_cname_record" {
+resource "google_dns_record_set" "cname_record" {
   count        = var.environment == "prod" ? 1 : 0
-  name         = "www.aishwaryabhargava.com."
+  name         = "www.aishwaryabhargava.com." # Replace with your actual domain
   managed_zone = google_dns_managed_zone.my_zone[0].name
   type         = "CNAME"
   ttl          = 300
 
-  rrdatas = ["c.storage.googleapis.com."]
+  rrdatas = ["c.storage.googleapis.com."] # GCS bucket URL for static website hosting
 }
 
-# CNAME Record for root domain (redirect to www)
-resource "google_dns_record_set" "root_cname_record" {
+resource "google_dns_record_set" "a_record" {
   count        = var.environment == "prod" ? 1 : 0
-  name         = "aishwaryabhargava.com."
+  name         = "aishwaryabhargava.com." # Replace with your actual domain
   managed_zone = google_dns_managed_zone.my_zone[0].name
-  type         = "CNAME"
+  type         = "A"
   ttl          = 300
 
-  rrdatas = ["www.aishwaryabhargava.com."]
+  rrdatas = ["216.239.32.21", "216.239.34.21", "216.239.36.21", "216.239.38.21"] # These are GCS IP addresses for static website hosting
 }
 
 resource "google_dns_record_set" "txt_record" {
